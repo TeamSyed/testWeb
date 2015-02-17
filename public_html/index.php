adc3ffec41017b331c6089191bd0741a2d96afcc
Kamran Syed <seitus@live.com>
2015-02-17 17:52:35 -0500

    public_html added, index.php moved

---
+++ public_html/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Testing</title>
+    </head>
+    <body>
+        313143
+        <?php
+       echo 'Testing 123';
+       echo 'Hello World';
+        ?>
+         <p> testing </p>
+    </body>
+</html>
